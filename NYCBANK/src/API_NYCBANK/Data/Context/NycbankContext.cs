20c46a2ff167270c31bb3f783e8d1048bccd1233
RenanVenturini <renanventurinioficial@gmail.com>
2023-10-28 12:07:18 -0300

    Estrutura do projeto

---
+++ NYCBANK/src/API_NYCBANK/Data/Context/NycbankContext.cs
@@ -0,0 +1,14 @@
+﻿using API_NYCBANK.Data.Context.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_NYCBANK.Data.Context
+{
+    public class NycbankContext : DbContext
+    {
+        public NycbankContext(DbContextOptions<NycbankContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
